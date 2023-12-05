b694ba7a05c71cc34c5e63cd53e488359dae9b1e
Simiams <simon.convert@epsi.fr>
2023-12-05 15:37:46 +0100

    A mort

---
+++ NolOusSim/src/main/java/fr/Arosaje/NolOusSim/NolOusSimApplication.java
@@ -0,0 +1,13 @@
+package fr.Arosaje.NolOusSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolOusSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolOusSimApplication.class, args);
+	}
+
+}
---
+++ NolOusSim/src/test/java/fr/Arosaje/NolOusSim/NolOusSimApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.Arosaje.NolOusSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolOusSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
